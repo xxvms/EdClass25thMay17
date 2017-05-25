1dde137192b4cb4166dc61de2adb8bba6431bb08
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-25 19:10:15 +0100

    progress

--- EdClass25thMay17/EdClass25thMay17.cpp
+++ EdClass25thMay17/EdClass25thMay17.cpp
@@ -15,7 +15,10 @@ public:
 	Part(int i_) : i(i_)
 	{
 		i = i_;
+
 	}
+	void push()
+	{	}
 };
 
 class Derived : public Part
@@ -26,14 +29,25 @@ public:
 	Derived() : j(2)
 	{}
 
-	Derived(int j_) : j(j_)
-	{}
+	Derived(int j_) : j(j_), Part(3)
+	{
+//		Part::Part(3);
+	}
+
+	void push()
+	{	
+		Part::push();
+	}
 
 };
 
 int main(void)
 {
 	Derived d1(4);
+	d1.push();
+
+	d1.Part::push();
+
 	return 0;
 }
 
